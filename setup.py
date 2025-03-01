9a6b6a2af41600630607cfc40ddaeff93d404cf9
Antton Alberdi <37664231+anttonalberdi@users.noreply.github.com>
2025-03-01 08:10:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arch3d",
+    version="1.0.0",
+    author="Antton Alberdi",
+    author_email="antton.alberdi@sund.ku.dk",
+    description="Python software for archiving 3D'omics data and metadata in public databases",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+        "argparse",
+        "json"
+    ],
+    entry_points={
+        "console_scripts": [
+            "arch3d=arch3d.cli:main",
+        ],
+    },
+    python_requires=">=3.6",
+)
