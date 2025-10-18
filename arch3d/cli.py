c25fe66092794ce9ddfd657c84ac06fc9fd9c83c
anttonalberdi <anttonalberdi@gmail.com>
2025-10-18 20:21:56 +0200

    limit number of snakemake connections

--- arch3d/cli.py
+++ arch3d/cli.py
@@ -46,13 +46,14 @@ def unlock_snakemake(output_dir, profile):
     subprocess.run(unlock_command, shell=False, check=True)
     print(f"The output directory {output_dir} has been succesfully unlocked.")
 
-def run_snakemake(workflow, output_dir, profile):
+def run_snakemake(workflow, output_dir, connections, profile):
     snakemake_command = [
         "/bin/bash", "-c",
         f"module load {config_vars['SNAKEMAKE_MODULE']} && "
         "snakemake "
         f"-s {PACKAGE_DIR / 'workflow' / 'Snakefile'} "
         f"--directory {output_dir} "
+        f"--jobs {connections} "
         f"--workflow-profile {PACKAGE_DIR / 'profile' / profile} "
         f"--configfile {CONFIG_PATH} "
         f"--config package_dir={PACKAGE_DIR} workflow={workflow} output_dir={output_dir}"
@@ -77,6 +78,7 @@ def main():
     subparser_macro.add_argument("-o", "--output", required=True, type=pathlib.Path, help="Output directory")
     subparser_macro.add_argument("-u", "--username", required=True, help="EBI Webin username. e.g, Webin-12345")
     subparser_macro.add_argument("-p", "--password", required=True, help="EBI Webin password")
+    subparser_macro.add_argument("-c", "--connections", required=False, default=16, help="Number of concurrent connections for uploading data")
 
     # Arguments for MICRO sample data
     subparser_micro = subparsers.add_parser("microsample", help="Upload micro-scale nucleotide data to ENA")
@@ -85,6 +87,7 @@ def main():
     subparser_micro.add_argument("-o", "--output", required=True, type=pathlib.Path, help="Output directory")
     subparser_micro.add_argument("-u", "--username", required=True, help="EBI Webin username. e.g, Webin-12345")
     subparser_micro.add_argument("-p", "--password", required=True, help="EBI Webin password")
+    subparser_micro.add_argument("-c", "--connections", required=False, default=16, help="Number of concurrent connections for uploading data")
 
     # Arguments for BioSample data
     subparser_animal = subparsers.add_parser("biosample", help="Upload specimen, segment of microsection metadata to BioSamples")
@@ -111,7 +114,7 @@ def main():
         create_run_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'run'))
         create_experiment_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'experiment'))
         create_sample_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'sample'))
-        run_snakemake(args.command, Path(args.output).resolve(), 'slurm')
+        run_snakemake(args.command, Path(args.output).resolve(),args.connections, 'slurm')
 
     if args.command == "microsample":
         input_dir = args.output / "input"
@@ -121,7 +124,7 @@ def main():
         create_run_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'run'))
         create_experiment_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'experiment'))
         create_microsample_checklists(args.metadata, str(Path(args.output).resolve() / 'checklists' / 'sample'))
-        run_snakemake(args.command, Path(args.output).resolve(), 'slurm')
+        run_snakemake(args.command, Path(args.output).resolve(),args.connections, 'slurm')
 
     if args.command == "biosample":
         process_biosample(args.input, args.output, args.username, args.password)
